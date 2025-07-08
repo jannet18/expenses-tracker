f487cd869e99fc48ad17eb6bebb63482ca67d856
jannet18 <janetngei18@gmail.com>
2025-07-08 16:07:49 +0300

    add:vite config

--- expense-ui/vite.config.js
+++ expense-ui/vite.config.js
@@ -1,17 +1,12 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://expenses-tracker-haqf.onrender.com",
-        changeOrigin: true,
-        secure: false,
-        // rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
   },
+  base: "./",
 });
